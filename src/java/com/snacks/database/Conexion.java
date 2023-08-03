74b9259161ceac9b490496d155b9a95d2e534d6b
Anthony Pérez Palacios <thonydev0726@gmail.com>
2023-08-02 22:58:15 -0500

    commit conexiones

---
+++ src/java/com/snacks/database/Conexion.java
@@ -0,0 +1,24 @@
+package com.snacks.database;
+
+import java.sql.*;
+
+public class Conexion {
+
+    public static Connection getConexion() {
+        Connection cn = null;
+        String DATA_BASE = "papeleria";
+        String USER = "root";
+        String HOST = "localhost";
+        String PASSWORD = "";
+        String PORT = ":3306/";
+        String URL = "jdbc:mysql://" + HOST + PORT + DATA_BASE + "?serverTimezone=UTC";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("ERROR DE CONEXION: " + e.getMessage());
+        }
+        return cn;
+    }
+
+}
---
+++ src/java/com/snacks/security/Encriptador.java
@@ -0,0 +1,52 @@
+package com.snacks.security;
+
+import java.security.*;
+import java.util.*;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+public class Encriptador {
+
+    String secretKey = "appSoft07262001";
+
+    public String encriptar(String cadena) {
+        String encriptacion = "";
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] llavePassword = md5.digest(secretKey.getBytes("utf-8"));
+            byte[] BytesKey = Arrays.copyOf(llavePassword, 24);
+            SecretKey key = new SecretKeySpec(BytesKey, "DESede");
+            Cipher cifrado = Cipher.getInstance("DESede");
+            cifrado.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainTextBytes = cadena.getBytes("utf-8");
+            byte[] buf = cifrado.doFinal(plainTextBytes);
+            byte[] base64Bytes = Base64.encodeBase64(buf);
+            encriptacion = new String(base64Bytes);
+        } catch (Exception ex) {
+            System.out.println("error al encriptar");
+        }
+        return encriptacion;
+    }
+
+    public String desencriptar(String cadenaEncriptada) {
+        String desencriptacion = "";
+        try {
+            byte[] message = Base64.decodeBase64(cadenaEncriptada.getBytes("utf-8"));
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] digestOfPassword = md5.digest(secretKey.getBytes("utf-8"));
+            byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+            SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+            Cipher decipher = Cipher.getInstance("DESede");
+            decipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] plainText = decipher.doFinal(message);
+            desencriptacion = new String(plainText, "UTF-8");
+
+        } catch (Exception ex) {
+            System.out.println("error al desencriptar");
+        }
+        return desencriptacion;
+    }
+    
+
+}
